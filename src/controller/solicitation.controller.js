f38649a2e714c63b3beb185719cde492ed29a6db
Bruno Koura <brunokoura@gmail.com>
2022-04-18 13:37:06 -0300

    add is new solicitation condition

--- src/controller/solicitation.controller.js
+++ src/controller/solicitation.controller.js
@@ -3,14 +3,20 @@ angular.module(APP_NAME).controller('SolicitationController', function($scope, s
 
     $scope.newSolicitation = {};
 
+    $scope.isNewProjectType = !!$scope.newSolicitation?.solicitationType?.id === 1;
+
     var loadSolicitationTypes = async function(){
-        $scope.solicitationTypes = await solicitationTypesService.getTypes()
+        $scope.solicitationTypes = await solicitationTypesService.getTypes();
         $scope.$apply();
     };
 
     async function addNewSolicitation(){
-        await solicitationService.registerNewSolicitation($scope.newSolicitation)
-    }
+        await solicitationService.registerNewSolicitation($scope.newSolicitation);
+    };
+
+    $scope.updateIsNewProjectType = function(){
+        $scope.isNewProjectType = $scope.newSolicitation?.solicitationType?.id === 1;
+    };
 
     $scope.addNewSolicitation = addNewSolicitation;
 
